06816a34534eaee52dc8d830246b1f694914a603
Waleed Nasir <waleeddotdev@gmail.com>
2025-03-20 19:37:53 +0500

    Update Ending.jsx

--- app/components/Ending.jsx
+++ app/components/Ending.jsx
@@ -9,7 +9,7 @@ const Ending = () => {
             </div>
             <div id="contact" className="flex py-20 flex-col gap-2 items-center justify-center">
                 <h4 className=" text-balance tracking-tight  leading-tight text-base hover:bg-white/80 transition-all duration-300 py-1 px-3 bg-white font-medium rounded-lg w-fit text-black">Contact</h4>
-                <h1 className="text-5xl text-center text-balance leading-tight tracking-tight font-bold">Get in Tocuh</h1>
+                <h1 className="text-5xl text-center text-balance leading-tight tracking-tight font-bold">Get in Touch</h1>
                 <h2 className="text-xl  text-center leading-tight"><span className="opacity-60">Want to chat? Just shoot me a dm with</span> <a href="https://instagram.com/waleeddotdev" className="text-[#ffffff] opacity-90 hover:opacity-60 duration-75 transition-all underline">a direct question on Instagram</a> <span className="opacity-60">and I'll respond whenever I can. I will ignore all soliciting.</span>
                 </h2>
             </div>
