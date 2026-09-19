24bb28ca7929105784e9eb980b23f00d859dddf2
waleeddotdev <waleeddotdev@gmail.com>
2026-09-19 18:18:07 +0500

    add: google search index veritification tag

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ const dm_sans = DM_Sans({ subsets: ['latin'] })
 export const metadata = {
     title: "Waleed Nasir | Portfolio",
     description: "From code to design, bringing concepts to life with simplicity, style, and passion",
+    verification: {
+        google: "tC2z_qZ5kLJxh-zhRvLN-2qABoyQcageYKYJ_-1YwrY",
+    },
 };
 
 export default function RootLayout({ children }) {
